2324035c3b0511e284fb0b9eae1700e615fc7f70
PranabM <drew.pranab7@gmail.com>
2023-02-09 20:42:45 -0700

    added the back end

---
+++ stockalertbackend/src/main/java/com/backend/stockalertbackend/StockalertbackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.stockalertbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockalertbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockalertbackendApplication.class, args);
+	}
+
+}
---
+++ stockalertbackend/src/test/java/com/backend/stockalertbackend/StockalertbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.stockalertbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockalertbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
